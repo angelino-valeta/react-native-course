a9291987e84547d7bc0f76ef419aa206a0c9f8a5
Angelino Valeta <angelinovaleta@gmail.com>
2022-12-28 18:03:19 +0100

    Add Style and render list

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,19 +4,27 @@ import { View, StyleSheet, Text, FlatList } from 'react-native';
 const ListScreen = () => {
   
   const friends = [
-    {name: 'Friend #1'},
-    {name: 'Friend #2'},
-    {name: 'Friend #3'},
-    {name: 'Friend #4'},
+    {name: 'Friend #1', age: 20},
+    {name: 'Friend #2', age: 45},
+    {name: 'Friend #3', age: 32},
+    {name: 'Friend #4', age: 27},
 ]
   
   return (
     <FlatList 
+    showsHorizontalScrollIndicator={false}
+    keyExtractor={friend => friend.name}
     data={friends}
     renderItem={({ item }) => {
-      return <Text>{item.name}</Text>
+      return <Text style={styles.textStyle} >{item.name} - Age {item.age}</Text>
     }}/>
   )
 }
+
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 4
+  }
+})
 
 export default ListScreen;
