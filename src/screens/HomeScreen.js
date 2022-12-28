55d178e8ecf4af4b8686499d0b6f9bddfe5a5906
Angelino Valeta <angelinovaleta@gmail.com>
2022-12-28 18:33:02 +0100

    Add button and call router components

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hi There</Text>
+      <Button
+        onPress={() => navigation.navigate('Component')}
+        title="Go to Components Demo"
+      />
+      <TouchableOpacity onPress={() => navigation.navigate('List')}>
+        <Text>Got to List Demo</Text>
+      </TouchableOpacity>
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
+    marginVertical: 10,
   },
 });
 
