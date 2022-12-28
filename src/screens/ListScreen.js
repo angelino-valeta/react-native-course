732024f93066f39526116a43c07928bb44205a9f
Angelino Valeta <angelinovaleta@gmail.com>
2022-12-28 17:31:45 +0100

    List

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const ListScreen = () => {
+  return (
+    <view>
+      <Text>List Screen Component</Text>
+    </view>
+  )
+}
+
+export default ListScreen;
