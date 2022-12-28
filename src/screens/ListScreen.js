d6cb5ac0794fb0f76b2d1fe43ffd961669e659d9
Angelino Valeta <angelinovaleta@gmail.com>
2022-12-28 17:47:38 +0100

    ListScreen

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, FlatList } from 'react-native';
 
 const ListScreen = () => {
+  
+  const friends = [
+    {name: 'Friend #1'},
+    {name: 'Friend #2'},
+    {name: 'Friend #3'},
+    {name: 'Friend #4'},
+]
+  
   return (
-    <view>
-      <Text>List Screen Component</Text>
-    </view>
+    <FlatList 
+    data={friends}
+    renderItem={({ item }) => {
+      return <Text>{item.name}</Text>
+    }}/>
   )
 }
 
